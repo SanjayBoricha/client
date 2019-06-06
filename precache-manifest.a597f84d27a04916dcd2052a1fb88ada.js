594cc86a4d910a1f38c69fec7acd8f03d26b80cd
SanjayBoricha <sanjayboricha1001@gmail.com>
2019-06-06 18:37:10 +0530

    Updates

---
+++ precache-manifest.a597f84d27a04916dcd2052a1fb88ada.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e8413d14409c16ba607a640c91bc19",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "8216d9d3615ce8dc0371",
+    "url": "/client/static/js/2.72703363.chunk.js"
+  },
+  {
+    "revision": "6d2295e5a44b431518ec",
+    "url": "/client/static/js/main.98a130a4.chunk.js"
+  },
+  {
+    "revision": "79f1a12bb5d1cdf91f65",
+    "url": "/client/static/js/runtime~main.3fca2621.js"
+  }
+]);
