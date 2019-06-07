911cef87f945c036f7dfa0dfccc771c0889bc0c1
SanjayBoricha <sanjayboricha1001@gmail.com>
2019-06-07 12:36:21 +0530

    Updates

---
+++ precache-manifest.b90ef622e89b40916043882850b88665.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a257ddacf59cb695350f754cfec9e0f",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "b274f890c951e29eb657",
+    "url": "/client/static/js/2.3f6af5ba.chunk.js"
+  },
+  {
+    "revision": "661d9de8522c7aaee4b1",
+    "url": "/client/static/js/main.56a6cf76.chunk.js"
+  },
+  {
+    "revision": "79f1a12bb5d1cdf91f65",
+    "url": "/client/static/js/runtime~main.3fca2621.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/client/precache-manifest.d105efd54542a62482f1074ab357da6c.js"
+  "/client/precache-manifest.b90ef622e89b40916043882850b88665.js"
 );
 
 self.addEventListener('message', (event) => {
